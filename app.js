89d0281aaf178bd376647fe4a3e822cf25f61bc2
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-05-29 13:54:14 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
@@ -13,6 +14,21 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res)=>{
   res.render('home');
 });
+
+app.get('/register', (req, res)=>{
+  res.render('register');
+});
+
+app.get('/login', (req, res)=>{
+  res.render('login');
+});
+
+app.get('/reviews', (req, res)=>{
+  res.render('reviews');
+});
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);

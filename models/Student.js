7d5f1926ae323c1d04c3440952d9993453f6d658
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-04-07 00:13:57 +0530

    add passport-local auth

--- models/Student.js
+++ models/Student.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const studentSchema = new mongoose.Schema({
     username: String,
@@ -7,6 +8,21 @@ const studentSchema = new mongoose.Schema({
 
 },{ timestamps: true });
 
+
+studentSchema.statics.createUser = async function(username, email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const newUser = new this({ username, email, password: hashedPassword });
+    return newUser.save();
+};
+
+
+studentSchema.statics.findByEmail = function(username) {
+    return this.findOne({ email: username });
+};
+
+studentSchema.methods.verifyPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
 
 // Create a model based on the schema
 const Student = mongoose.model('Student', studentSchema);
